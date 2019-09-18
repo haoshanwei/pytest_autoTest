b23765982f8e6f0219ebfadf9c0d064241be3df0
孟祥国 <mxgnene01@gmail.com>
2019-09-18 16:30:49 +0800

    基于pytest 框架的接口自动化平台

--- conf/sysconfig.py
+++ conf/sysconfig.py
@@ -16,7 +16,6 @@ from rediscluster import RedisCluster
 
 
 UC_DB = records.Database('postgres://daling_app_admin:1234@Daling@a.pgsql.qa.daling.com:5410/sbc_ucenter_db')
-# UC_DB = records.Database('postgres://pgsql:oe6Imt570Q6I2ZLd@l-db6.ops.bj2.daling.com:5495/sbc_ucenter_db')
 UC_REDIS = redis.Redis('redis://a-redis1.qa.bj4.daling.com:6379')
 UC_REDIS_CLUSTER = RedisCluster(startup_nodes=[{"host": "a-redis1.qa.bj4.daling.com", "port": 6380},
                                 {"host": "a-redis1.qa.bj4.daling.com", "port": 6381},
@@ -27,4 +26,8 @@ UC_REDIS_CLUSTER = RedisCluster(startup_nodes=[{"host": "a-redis1.qa.bj4.daling.
                                  ])
 
 UC_HOST = 'http://a.xc.qa.daling.com'
+
+
+# PORD
+# UC_DB = records.Database('postgres://pgsql:oe6Imt570Q6I2ZLd@l-db6.ops.bj2.daling.com:5495/sbc_ucenter_db')
 # UC_HOST = 'http://xc.srv.daling.com'
