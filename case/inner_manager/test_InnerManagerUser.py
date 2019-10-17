837c835f9c753995ee80e971069c535c3871043f
孟祥国 <mxgnene01@gmail.com>
2019-10-18 04:53:18 +0800

    增加用户中心 新接口的case

--- case/inner_manager/test_InnerManagerUser.py
+++ case/inner_manager/test_InnerManagerUser.py
@@ -13,7 +13,6 @@
 import pytest
 import requests
 from dutil.res_diff import res_diff
-from conf.sysconfig import UC_HOST
 from dutil.find_case import findCase
 from dutil.make_ddt import MakeDdt
 
@@ -21,21 +20,6 @@ casepath = findCase(__file__, 'uc_inner_device.yml', n=2)
 test_cases = MakeDdt(casepath).makeData()
 
 class TestInnerManagerUser():
-    '''
-    测试更新与数据库对比的情况
-    '''
-    def test_updateShopName(self, uc_db):
-        '''更新用户店铺头像'''
-        url = UC_HOST + '/xc_uc/inner/manager/shop/updateShopName.do'
-        shopId = 588975
-        shopName = '孟祥国A环境'
-        params = dict(shopId=shopId, shopName=shopName)
-
-        requests.request('POST', url, params=params)
-        db_shopName = uc_db.query("select name from t_shop where id = {}".format(shopId)).first().name
-        assert shopName == db_shopName
-
-
     @pytest.mark.parametrize("method, url, params, data, headers, cookies, proxies, status_code, expectData",
                              test_cases)
     def test_success(self, method, url, params, data, headers, cookies, proxies, status_code, expectData):
--- case/inner_manager/test_innerManagerShop.py
+++ case/inner_manager/test_innerManagerShop.py
@@ -15,12 +15,28 @@ import requests
 from dutil.res_diff import res_diff
 from dutil.find_case import findCase
 from dutil.make_ddt import MakeDdt
+from conf.sysconfig import UC_HOST
 
 casepath = findCase(__file__, 'uc_inner_manager_shop.yml', n=2)
 app_stock_cases = MakeDdt(casepath).makeData()
 
 
 class TestUcenterInnerManagerShop():
+    '''
+        测试更新与数据库对比的情况
+        '''
+
+    def test_updateShopName(self, uc_db):
+        '''更新用户店铺头像'''
+        url = UC_HOST + '/xc_uc/inner/manager/shop/updateShopName.do'
+        shopId = 588975
+        shopName = '孟祥国A环境'
+        params = dict(shopId=shopId, shopName=shopName)
+
+        requests.request('POST', url, params=params)
+        db_shopName = uc_db.query("select name from t_shop where id = {}".format(shopId)).first().name
+        assert shopName == db_shopName
+
     '''
     基于 yaml 文件数据的自动化case
     '''
