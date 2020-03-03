b8148385164d0e154f9a9a0bb2ded176284ef8d8
孟祥国 <mxgnene01@gmail.com>
2020-03-03 12:20:02 +0800

    修改配置文件

--- conf/sysconfig.py
+++ conf/sysconfig.py
@@ -16,13 +16,13 @@ from rediscluster import RedisCluster
 
 
 UC_DB = records.Database('postgres://pgsql:oe6Imt570Q6I2ZLd@l-db1.beta.bj4.daling.com:5495/sbc_shop_db')
-UC_REDIS = redis.Redis('redis://a-redis1.qa.bj4.daling.com:6379')
-UC_REDIS_CLUSTER = RedisCluster(startup_nodes=[{"host": "a-redis1.qa.bj4.daling.com", "port": 6380},
-                                {"host": "a-redis1.qa.bj4.daling.com", "port": 6381},
-                                {"host": "a-redis1.qa.bj4.daling.com", "port": 6382},
-                                {"host": "a-redis1.qa.bj4.daling.com", "port": 6383},
-                                {"host": "a-redis1.qa.bj4.daling.com", "port": 6384},
-                                {"host": "a-redis1.qa.bj4.daling.com", "port": 6385}
+UC_REDIS = redis.Redis('redis://a-redis1.qa.bj6.daling.com:6379')
+UC_REDIS_CLUSTER = RedisCluster(startup_nodes=[{"host": "a-redis1.qa.bj6.daling.com", "port": 6380},
+                                {"host": "a-redis1.qa.bj6.daling.com", "port": 6381},
+                                {"host": "a-redis1.qa.bj6.daling.com", "port": 6382},
+                                {"host": "a-redis1.qa.bj6.daling.com", "port": 6383},
+                                {"host": "a-redis1.qa.bj6.daling.com", "port": 6384},
+                                {"host": "a-redis1.qa.bj6.daling.com", "port": 6385}
                                  ])
 
 UC_HOST = 'http://atxc.beta.daling.com'
